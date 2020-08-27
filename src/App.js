f7ce4f280be6d8cf21ce6a062c3758df08950b1f
ParkSeongju <parksj914@naver.com>
2020-08-27 17:28:52 +0900

    styledCompnents-css덮어쓰기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./App.scss"
 import styled, {ThemeProvider} from 'styled-components';
 import Button from './components/Button/Button';
+import Dialog from "./components/dialog/Dialog";
 
 const AppBlock = styled.div`
     width: 512px;
@@ -28,11 +29,20 @@ function App() {
               }
           }}
       >
-          <AppBlock>
-              <Button size="large">BUTTON</Button>
-              <Button color="gray" size="medium" outline>BUTTON</Button>
-              <Button color="pink" size="small" fullWidth>BUTTON</Button>
-          </AppBlock>
+          <>
+              <AppBlock>
+                  <Button size="large">BUTTON</Button>
+                  <Button color="gray" size="medium" outline>BUTTON</Button>
+                  <Button color="pink" size="small" fullWidth>BUTTON</Button>
+              </AppBlock>
+              <Dialog
+                  title="정말로 삭제하시겠습니까?"
+                  confirmText="삭제"
+                  cancelText="취소"
+              >
+                  데이터를 정말로 삭제하시겠습니까?
+              </Dialog>
+          </>
       </ThemeProvider>
   );
 }
