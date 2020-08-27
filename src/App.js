855ec8eefc5648d7d8619ccf2a4bc07d4493ee90
ParkSeongju <parksj914@naver.com>
2020-08-27 15:45:30 +0900

    cssModule

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import React from 'react';
-import Plus from "./components/plus/Plus";
+import React ,{useState}from 'react';
 import "./App.scss"
+import CheckBox from "./components/checkbox/CheckBox";
 
 function App() {
+    const [check, setCheck] = useState(false);
+    const onChange = e =>{
+        setCheck(e.target.checked);
+    };
   return (
-      <div className="App">
-          <div className="plus">
-              {/*추가해야 할 것 :
-                size, color, outline, fullWidth
-              */}
-              <Plus
-                  color="lime"
-                  size="large"
-                  onClick={()=>console.log('클릭됨')}
-              >
-                  Plus
-              </Plus>
-              <Plus
-                  color="yellow"
-                  size="medium"
-                  outline
-              >
-                  Plus
-              </Plus>
-              <Plus
-                  color="orange"
-                  size="small"
-                  fullWidth
-              >
-                  Plus
-              </Plus>
-          </div>
+      <div>
+          <CheckBox
+              onChange={onChange}
+              checked={check}
+          >
+              다음 약관에 모두 동의
+          </CheckBox>
+          <p>
+              <b>check: </b>
+              {check ? 'true' : 'false'}
+          </p>
       </div>
   );
 }
