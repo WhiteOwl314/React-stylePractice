05bf9d1b8cf1515739468dc45221e14533f913a6
ParkSeongju <parksj914@naver.com>
2020-08-27 18:29:20 +0900

    styledCompnents-dialog-사라지는에니메이션-에니메이션 추가

--- src/components/dialog/Dialog.js
+++ src/components/dialog/Dialog.js
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import styled, {keyframes} from 'styled-components';
+import styled, {keyframes, css} from 'styled-components';
 import Button from '../Button/Button';
 
 const fadeIn = keyframes`
@@ -11,12 +11,30 @@ const fadeIn = keyframes`
     }
 `;
 
+const fadeOut = keyframes`
+    from{
+        opacty: 1
+    }
+    to{
+        opacity: 0
+    }
+`;
+
 const slideUp = keyframes`
     from{
         transform: translateY(200px);
     }
     to{
         transform: translateY(0px);
+    }
+`;
+
+const slideDown = keyframes`
+    from{
+        transform: translateY(0px);
+    }
+    to{
+        transform: translateY(200px);
     }
 `;
 
@@ -35,6 +53,13 @@ const DarkBackground = styled.div`
     animation-timing-function: ease-out;
     animation-name: ${fadeIn};
     animation-fill-mode: forwards;
+    
+    ${props =>
+        props.disappear &&
+        css`
+            animation-name: ${fadeOut};
+        `
+    }
 `;
 
 const DialogBlock = styled.div`
@@ -54,6 +79,13 @@ const DialogBlock = styled.div`
     animation-timing-function: ease-out;
     animation-name: ${slideUp};
     animation-fill-mode: forwards;
+    
+    ${props =>
+        props.disappear &&
+        css`
+            animation-name: ${slideDown};
+        `
+    }
 `;
 
 const ButtonGroup = styled.div`
@@ -87,13 +119,15 @@ function Dialog({
                 setTimeout(()=>setAnimate(false), 250);
             }
             setLocalVisible(visible);
-        }, [localVisible, visible]
+        },
+        [localVisible, visible]
     );
 
     if (!animate && !localVisible) return null;
+
     return(
-        <DarkBackground>
-            <DialogBlock>
+        <DarkBackground disappear={!visible}>
+            <DialogBlock disappear={!visible}>
                 <h3>{title}</h3>
                 <p>{children}</p>
                 <ButtonGroup>
